a4dc8fd3f7309379ed6584c4d5510166578b23de
Mariana Junqueira <69199400+junqueiras@users.noreply.github.com>
2020-10-29 20:06:49 -0300

    cores alteradas

--- app/src/main/java/com/example/colormyview/MainActivity.kt
+++ app/src/main/java/com/example/colormyview/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.colormyview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    var currentColor = R.color.grey // para ser válida em toda a classe
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setColorButtonsAction()
+        }
+
+     private fun setColorButtonsAction() {
+        redButton.setOnClickListener {
+            currentColor = R.color.red
+        }
+
+         yellowButton.setOnClickListener {
+             currentColor = R.color.yellow
+         }
+
+         greenButton.setOnClickListener {
+             currentColor = R.color.green
+         }
     }
 }
